61a26ee735557d0d4d5aab57354b5a57cf9aec41
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2022-05-11 17:31:26 +0300

    show memory usage

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -4,46 +4,83 @@ Monitor::Monitor(QObject *parent)
     : QObject(parent)
     , m_uptime(QString(""))
     , m_hostname(QString(""))
+    , m_memoryUsage(QString(""))
+    , m_PCStartSeconds(0)
+    , totalram(0)
+    , freeram (0)
+    , sharedram(0)
+    , totalswap(0)
+    , freeswap (0)
+    , memoryUsagePercent (0.0)
 {
+#ifdef Q_OS_ANDROID
+    //https://github.com/mzlogin/CleanExpert/blob/master/app/src/main/java/org/mazhuang/cleanexpert/util/MemStat.java
+    //https://github.com/jaredrummler/AndroidDeviceNames
+
+#elif defined Q_OS_LINUX
+    m_hostname = QSysInfo::machineHostName();
+#endif
     updateSystemInfo();
 }
 
-QString Monitor::getUptime() const
+const QString &Monitor::getUptime() const
 {
     return m_uptime;
 }
 
-QString Monitor::getHostname() const
+const QString &Monitor::getHostname() const
 {
     return m_hostname;
 }
+const QString &Monitor::getMemoryUsage() const
+{
+    return m_memoryUsage;
+}
 
+// ------------------------------- SLOTS -------------------------------------
 void Monitor::updateSystemInfo()
 {
 #ifdef Q_OS_ANDROID
 
 #elif defined Q_OS_LINUX
-    qDebug() << "tic";
     struct sysinfo s_info;
-    long m_nPCStartSeconds = 0;
 
     if ( 0 == sysinfo(&s_info)){
-        m_nPCStartSeconds = s_info.uptime;
-        int days = round(m_nPCStartSeconds / (60 * 60 * 24) );
-        int hours = round((m_nPCStartSeconds - days*60*60*24) / (60 * 60));
-        int mins = round((m_nPCStartSeconds - days*60*60*24 - hours*60*60) / 60);
-        int sec = round(m_nPCStartSeconds - days*60*60*24 - hours*60*60 - mins*60);
 
-        m_uptime   = QString("%1d %2h:%3m:%4s")
-                .arg(days, 2, 10, QChar('0'))
-                .arg(hours,2, 10, QChar('0'))
-                .arg(mins,2, 10, QChar('0'))
-                .arg(sec,2, 10, QChar('0'));
-        m_hostname = QSysInfo::machineHostName();
-        emit uptimeChanged();
+        m_PCStartSeconds = s_info.uptime;
+
+        totalram  = s_info.totalram;
+        freeram   = s_info.freeram;
+        sharedram = s_info.sharedram;
+        totalswap = s_info.totalswap;
+        freeswap  = s_info.freeswap;
+
+        updateUpTime( );
+        updateMemory( );
     }
 
 #endif
 }
 
+void Monitor::updateUpTime()
+{
+    int secondsInDay = 60 * 60 * 24;
+    int days  = round(   m_PCStartSeconds / (secondsInDay) );
+    int hours = round( (m_PCStartSeconds - days * secondsInDay) / ( 60 * 60 ) );
+    int mins  = round( (m_PCStartSeconds - days * secondsInDay - hours * 60 * 60 ) / 60);
+    int sec   = round(  m_PCStartSeconds - days * secondsInDay - hours * 60 * 60 - mins * 60);
 
+    m_uptime   = tr("%1d %2h:%3m:%4s")
+            .arg(days, 2, 10, QChar('0'))
+            .arg(hours,2, 10, QChar('0'))
+            .arg(mins, 2, 10, QChar('0'))
+            .arg(sec,  2, 10, QChar('0'));
+    emit uptimeChanged();
+}
+
+void Monitor::updateMemory()
+{
+    double percent = 100 - qRound((qreal)freeram / (qreal)totalram * 100.0f);
+    m_memoryUsage = tr("%1 \%").arg( percent );
+    emit memoryUsageChanged();
+}
--- src/monitor.h
+++ src/monitor.h
@@ -23,21 +23,36 @@ class Monitor : public QObject {
     Q_OBJECT
     //Q_PROPERTY(int moves READ moves WRITE setMoves NOTIFY movesChanged);
     Q_PROPERTY(QString uptime READ getUptime NOTIFY uptimeChanged);
+    Q_PROPERTY(QString memoryUsage READ getMemoryUsage NOTIFY memoryUsageChanged);
     Q_PROPERTY(QString hostname READ getHostname CONSTANT);
     //Q_PROPERTY(int tiles READ tiles CONSTANT)
 public:
     explicit Monitor(QObject *parent = nullptr);
 
-    QString getUptime() const;
-    QString getHostname() const;
+    const QString &getUptime() const;
+    const QString &getHostname() const;
+    const QString &getMemoryUsage() const;
 
 public slots:
-
     Q_INVOKABLE void updateSystemInfo();
+
 signals:
     void uptimeChanged();
+    void memoryUsageChanged();
+
 private:
     QString m_uptime;
     QString m_hostname;
+    QString m_memoryUsage;
+    quint64 m_PCStartSeconds;
+    quint64 totalram;	/* Total usable main memory size */
+    quint64 freeram;    /* Available memory size */
+    quint64 sharedram;	/* Amount of shared memory */
+    quint64 totalswap;	/* Total swap space size */
+    quint64 freeswap ;  /* swap space still available */
+    double  memoryUsagePercent;
 
+    void updateUpTime( );
+    void updateMemory( );
+   void bytesToKiloMegaGiga(quint64 m_bytes);
 };
